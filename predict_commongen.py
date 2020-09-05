ac796c83c01699917110b7f49cff0f9860991efd
Ravi Kiran Selvam <sravikiran0606@gmail.com>
2020-09-05 09:51:06 -0700

    Use the commandline arguments for prediction

--- predict_commongen.py
+++ predict_commongen.py
@@ -14,43 +14,78 @@ from transformers import (
     get_linear_schedule_with_warmup
 )
 
+def set_seed(seed):
+    random.seed(seed)
+    np.random.seed(seed)
+    torch.manual_seed(seed)
+    if torch.cuda.is_available():
+        torch.cuda.manual_seed_all(seed)
+
 def run():
     #torch.multiprocessing.freeze_support()
-    def set_seed(seed):
-        random.seed(seed)
-        np.random.seed(seed)
-        torch.manual_seed(seed)
-        if torch.cuda.is_available():
-            torch.cuda.manual_seed_all(seed)
-
     set_seed(42)
-    args_dict = dict(
-        data_dir="",  # path for data files
-        output_dir="",  # path to save the checkpoints
-        model_name_or_path='t5-base',
-        checkpoint_dir='',  # checkpoint_dir
-        tokenizer_name_or_path='t5-base',
-        nsp_generate=False,
-        max_seq_length=128,
-        learning_rate=3e-4,
-        weight_decay=0.0,
-        adam_epsilon=1e-8,
-        warmup_steps=0,
-        train_batch_size=4,
-        eval_batch_size=4,
-        num_train_epochs=2,
-        gradient_accumulation_steps=16,
-        n_gpu=1,
-        early_stop_callback=False,
-        fp_16=True,  # if you want to enable 16-bit training then install apex and set this to true
-        opt_level='O1',
-        # you can find out more on optimisation levels here https://nvidia.github.io/apex/amp.html#opt-levels-and-properties
-        max_grad_norm=1.0,  # if you enable 16-bit training then set this to a sensible value, 0.5 is a good default
-        seed=42,
-    )
 
-    args_dict.update({'data_dir': 'commongen', 'output_dir': 'commongen_concept_output_epoch10', 'num_train_epochs': 10})
-    args = argparse.Namespace(**args_dict)
+    parser = argparse.ArgumentParser()
+
+    parser.add_argument('--data_dir', type=str, default="datasets/commongen",
+                        help='Path for Data files')
+    parser.add_argument('--output_dir', type=str, default="outputs/commongen_concept_output_epoch10",
+                        help='Path to save the checkpoints')
+    parser.add_argument('--checkpoint_dir', type=str, default="",
+                        help='Checkpoint directory')
+
+    parser.add_argument('--model_name_or_path', type=str, default="t5-base",
+                        help='Model name or Path')
+    parser.add_argument('--tokenizer_name_or_path', type=str, default="t5-base",
+                        help='Tokenizer name or Path')
+    parser.add_argument('--nsp_generate', type=lambda x: (str(x).lower() == 'true'), default="False",
+                        help='Whether to generate NSP?')
+
+    # you can find out more on optimisation levels here https://nvidia.github.io/apex/amp.html#opt-levels-and-properties
+    parser.add_argument('--opt_level', type=str, default="01",
+                        help='Optimization level')
+    parser.add_argument('--early_stop_callback', type=lambda x: (str(x).lower() == 'true'), default="False",
+                        help='Whether to do early stopping?')
+
+    # if you want to enable 16-bit training then install apex and set this to true
+    parser.add_argument('--fp_16', type=lambda x: (str(x).lower() == 'true'), default="True",
+                        help='Whether to use 16 bit precision floating point operations?')
+
+    parser.add_argument('--learning_rate', type=float, default=3e-4,
+                        help='Learning Rate')
+    parser.add_argument('--weight_decay', type=float, default=0.0,
+                        help='Weight decay')
+    parser.add_argument('--adam_epsilon', type=float, default=1e-8,
+                        help='Epsilon value for Adam Optimizer')
+
+    # if you enable 16-bit training then set this to a sensible value, 0.5 is a good default
+    parser.add_argument('--max_grad_norm', type=float, default=1.0,
+                        help='Maximum Gradient Norm value for Clipping')
+
+    parser.add_argument('--max_seq_length', type=int, default=128,
+                        help='Maximum Sequence Length')
+    parser.add_argument('--warmup_steps', type=int, default=0,
+                        help='Number of warmup steps')
+    parser.add_argument('--train_batch_size', type=int, default=4,
+                        help='Batch size for Training')
+    parser.add_argument('--eval_batch_size', type=int, default=4,
+                        help='Batch size for Evaluation')
+    parser.add_argument('--num_train_epochs', type=int, default=10,
+                        help='Number of Training epochs')
+    parser.add_argument('--gradient_accumulation_steps', type=int, default=16,
+                        help='Gradient Accumulation Steps')
+    parser.add_argument('--n_gpu', type=str, default="1",
+                        help='GPU numbers to use for computation')
+    parser.add_argument('--seed', type=int, default=42,
+                        help='Manual Seed Value')
+
+    args = parser.parse_args()
+    print(args)
+
+    # Create a folder if output_dir doesn't exists:
+    if not os.path.exists(args.output_dir):
+        os.makedirs(args.output_dir)
+        print("Creating output directory")
 
     checkpoints = list(sorted(glob.glob(os.path.join(args.output_dir, "checkpointcheckpoint_ckpt_epoch_*.ckpt"), recursive=True)))
     print(str(checkpoints))
@@ -58,9 +93,9 @@ def run():
     model = T5FineTuner.load_from_checkpoint(checkpoints[-1])
 
     tokenizer = T5Tokenizer.from_pretrained(args.model_name_or_path)
-    test_examples = [x.rstrip() for x in open('./commongen/test.source').readlines()]
+    test_examples = [x.rstrip() for x in open(os.path.join(args.data_dir, 'test.source')).readlines()]
     test_fout = open('test.txt','w')
-    val_examples = [x.rstrip() for x in open('./commongen/valid.source').readlines()]
+    val_examples = [x.rstrip() for x in open(os.path.join(args.data_dir, 'valid.source')).readlines()]
     val_fout = open('val.txt','w')
 
     max_length = 24
