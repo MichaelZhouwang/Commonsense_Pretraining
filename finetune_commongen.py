864f2296049a09bd6ee5526c2401cc65534fd2b4
Ravi Kiran Selvam <sravikiran0606@gmail.com>
2020-09-05 08:59:07 -0700

    Use the commandline arguments for finetuning

--- finetune_commongen.py
+++ finetune_commongen.py
@@ -3,47 +3,82 @@ import random
 import numpy as np
 import torch
 import argparse
+import os
 import pytorch_lightning as pl
 from trainer import *
 
+def set_seed(seed):
+    random.seed(seed)
+    np.random.seed(seed)
+    torch.manual_seed(seed)
+    if torch.cuda.is_available():
+        torch.cuda.manual_seed_all(seed)
+
 def run():
     #torch.multiprocessing.freeze_support()
-    def set_seed(seed):
-        random.seed(seed)
-        np.random.seed(seed)
-        torch.manual_seed(seed)
-        if torch.cuda.is_available():
-            torch.cuda.manual_seed_all(seed)
-
     set_seed(42)
-    args_dict = dict(
-        data_dir="",  # path for data files
-        output_dir="",  # path to save the checkpoints
-        model_name_or_path='t5-base',
-        checkpoint_dir='t5_concept', #checkpoint_dir
-        tokenizer_name_or_path='t5-base',
-        max_source_length = 32,
-        max_target_length = 32,
-        learning_rate=2e-5,
-        weight_decay=0.0,
-        adam_epsilon=1e-8,
-        warmup_steps=400,
-        train_batch_size=4,
-        eval_batch_size=4,
-        num_train_epochs=10,
-        gradient_accumulation_steps=32,
-        n_gpu = 3,
-        early_stop_callback=False,
-        fp_16=False,  # if you want to enable 16-bit training then install apex and set this to true
-        opt_level='O1',
-        # you can find out more on optimisation levels here https://nvidia.github.io/apex/amp.html#opt-levels-and-properties
-        max_grad_norm=1.0,  # if you enable 16-bit training then set this to a sensible value, 0.5 is a good default
-        seed=42,
-    )
 
-    args_dict.update({'data_dir': 'commongen', 'output_dir': 'commongen_concept_output_epoch10', 'num_train_epochs': 10})
-    args = argparse.Namespace(**args_dict)
-    print(args_dict)
+    parser = argparse.ArgumentParser()
+
+    parser.add_argument('--data_dir', type=str, default="datasets/commongen",
+                        help='Path for Data files')
+    parser.add_argument('--output_dir', type=str, default="outputs/commongen_concept_output_epoch10",
+                        help='Path to save the checkpoints')
+    parser.add_argument('--checkpoint_dir', type=str, default="t5_concept",
+                        help='Checkpoint directory')
+
+    parser.add_argument('--model_name_or_path', type=str, default="t5-base",
+                        help='Model name or Path')
+    parser.add_argument('--tokenizer_name_or_path', type=str, default="t5-base",
+                        help='Tokenizer name or Path')
+
+    # you can find out more on optimisation levels here https://nvidia.github.io/apex/amp.html#opt-levels-and-properties
+    parser.add_argument('--opt_level', type=str, default="01",
+                        help='Optimization level')
+    parser.add_argument('--early_stop_callback', type=lambda x: (str(x).lower() == 'true'), default="False",
+                        help='Whether to do early stopping?')
+
+    # if you want to enable 16-bit training then install apex and set this to true
+    parser.add_argument('--fp_16', type=lambda x: (str(x).lower() == 'true'), default="False",
+                        help='Whether to use 16 bit precision floating point operations?')
+
+    parser.add_argument('--learning_rate', type=float, default=2e-5,
+                        help='Learning Rate')
+    parser.add_argument('--weight_decay', type=float, default=0.0,
+                        help='Weight decay')
+    parser.add_argument('--adam_epsilon', type=float, default=1e-8,
+                        help='Epsilon value for Adam Optimizer')
+
+    # if you enable 16-bit training then set this to a sensible value, 0.5 is a good default
+    parser.add_argument('--max_grad_norm', type=float, default=1.0,
+                        help='Maximum Gradient Norm value for Clipping')
+
+    parser.add_argument('--max_source_length', type=int, default=32,
+                        help='Maximum Source Length')
+    parser.add_argument('--max_target_length', type=int, default=32,
+                        help='Maximum Target Length')
+    parser.add_argument('--warmup_steps', type=int, default=400,
+                        help='Number of warmup steps')
+    parser.add_argument('--train_batch_size', type=int, default=4,
+                        help='Batch size for Training')
+    parser.add_argument('--eval_batch_size', type=int, default=4,
+                        help='Batch size for Evaluation')
+    parser.add_argument('--num_train_epochs', type=int, default=10,
+                        help='Number of Training epochs')
+    parser.add_argument('--gradient_accumulation_steps', type=int, default=32,
+                        help='Gradient Accumulation Steps')
+    parser.add_argument('--n_gpu', type=str, default="1,2,3",
+                        help='GPU numbers to use for computation')
+    parser.add_argument('--seed', type=int, default=42,
+                        help='Manual Seed Value')
+
+    args = parser.parse_args()
+    print(args)
+
+    # Create a folder if output_dir doesn't exists:
+    if not os.path.exists(args.output_dir):
+        os.makedirs(args.output_dir)
+        print("Creating output directory")
 
     checkpoint_callback = pl.callbacks.ModelCheckpoint(
         filepath=args.output_dir, prefix="checkpoint", monitor="val_loss", mode="min", save_top_k=5
@@ -51,9 +86,9 @@ def run():
 
     train_params = dict(
         accumulate_grad_batches=args.gradient_accumulation_steps,
-        gpus="1,2,3",
+        gpus=args.n_gpu,
         max_epochs=args.num_train_epochs,
-        early_stop_callback=False,
+        early_stop_callback=args.early_stop_callback,
         precision=16 if args.fp_16 else 32,
         amp_level=args.opt_level,
         gradient_clip_val=args.max_grad_norm,
--- finetune_generate.py
+++ finetune_generate.py
@@ -3,49 +3,86 @@ import random
 import numpy as np
 import torch
 import argparse
+import os
 import pytorch_lightning as pl
 from dataset import NSPDataset
 from trainer import *
 
+def set_seed(seed):
+    random.seed(seed)
+    np.random.seed(seed)
+    torch.manual_seed(seed)
+    if torch.cuda.is_available():
+        torch.cuda.manual_seed_all(seed)
+
 def run():
     #torch.multiprocessing.freeze_support()
-    def set_seed(seed):
-        random.seed(seed)
-        np.random.seed(seed)
-        torch.manual_seed(seed)
-        if torch.cuda.is_available():
-            torch.cuda.manual_seed_all(seed)
-
     set_seed(42)
-    args_dict = dict(
-        data_dir="",  # path for data files
-        output_dir="",  # path to save the checkpoints
-        model_name_or_path='t5-base',
-        checkpoint_dir='',  # checkpoint_dir
-        tokenizer_name_or_path='t5-base',
-        nsp_generate=True,
-        concept_generate = False,
-        max_seq_length=128,
-        learning_rate=3e-4,
-        weight_decay=0.0,
-        adam_epsilon=1e-8,
-        warmup_steps=0,
-        train_batch_size=4,
-        eval_batch_size=4,
-        num_train_epochs=2,
-        gradient_accumulation_steps=16,
-        n_gpu=4,
-        early_stop_callback=False,
-        fp_16=True,  # if you want to enable 16-bit training then install apex and set this to true
-        opt_level='O1',
-        # you can find out more on optimisation levels here https://nvidia.github.io/apex/amp.html#opt-levels-and-properties
-        max_grad_norm=1.0,  # if you enable 16-bit training then set this to a sensible value, 0.5 is a good default
-        seed=42,
-    )
 
-    args_dict.update({'data_dir': 'wikitext-2-raw', 'output_dir': 'nsp_new_wiki_generate', 'num_train_epochs': 2})
-    args = argparse.Namespace(**args_dict)
-    print(args_dict)
+    parser = argparse.ArgumentParser()
+
+    parser.add_argument('--data_dir', type=str, default="datasets/wikitext-2-raw",
+                        help='Path for Data files')
+    parser.add_argument('--output_dir', type=str, default="outputs/nsp_new_wiki_generate",
+                        help='Path to save the checkpoints')
+    parser.add_argument('--checkpoint_dir', type=str, default="",
+                        help='Checkpoint directory')
+
+    parser.add_argument('--model_name_or_path', type=str, default="t5-base",
+                        help='Model name or Path')
+    parser.add_argument('--tokenizer_name_or_path', type=str, default="t5-base",
+                        help='Tokenizer name or Path')
+
+    parser.add_argument('--nsp_generate', type=lambda x: (str(x).lower() == 'true'), default="True",
+                        help='Whether to generate NSP?')
+    parser.add_argument('--concept_generate', type=lambda x: (str(x).lower() == 'true'), default="False",
+                        help='Whether to do generate Concept?')
+
+    # you can find out more on optimisation levels here https://nvidia.github.io/apex/amp.html#opt-levels-and-properties
+    parser.add_argument('--opt_level', type=str, default="01",
+                        help='Optimization level')
+    parser.add_argument('--early_stop_callback', type=lambda x: (str(x).lower() == 'true'), default="False",
+                        help='Whether to do early stopping?')
+
+    # if you want to enable 16-bit training then install apex and set this to true
+    parser.add_argument('--fp_16', type=lambda x: (str(x).lower() == 'true'), default="True",
+                        help='Whether to use 16 bit precision floating point operations?')
+
+    parser.add_argument('--learning_rate', type=float, default=3e-4,
+                        help='Learning Rate')
+    parser.add_argument('--weight_decay', type=float, default=0.0,
+                        help='Weight decay')
+    parser.add_argument('--adam_epsilon', type=float, default=1e-8,
+                        help='Epsilon value for Adam Optimizer')
+
+    # if you enable 16-bit training then set this to a sensible value, 0.5 is a good default
+    parser.add_argument('--max_grad_norm', type=float, default=1.0,
+                        help='Maximum Gradient Norm value for Clipping')
+
+    parser.add_argument('--max_seq_length', type=int, default=128,
+                        help='Maximum Sequence Length')
+    parser.add_argument('--warmup_steps', type=int, default=0,
+                        help='Number of warmup steps')
+    parser.add_argument('--train_batch_size', type=int, default=4,
+                        help='Batch size for Training')
+    parser.add_argument('--eval_batch_size', type=int, default=4,
+                        help='Batch size for Evaluation')
+    parser.add_argument('--num_train_epochs', type=int, default=2,
+                        help='Number of Training epochs')
+    parser.add_argument('--gradient_accumulation_steps', type=int, default=16,
+                        help='Gradient Accumulation Steps')
+    parser.add_argument('--n_gpu', type=str, default="2,3,4,5",
+                        help='GPU numbers to use for computation')
+    parser.add_argument('--seed', type=int, default=42,
+                        help='Manual Seed Value')
+
+    args = parser.parse_args()
+    print(args)
+
+    # Create a folder if output_dir doesn't exists:
+    if not os.path.exists(args.output_dir):
+        os.makedirs(args.output_dir)
+        print("Creating output directory")
 
     checkpoint_callback = pl.callbacks.ModelCheckpoint(
         filepath=args.output_dir, prefix="checkpoint", monitor="val_loss", mode="min", save_top_k=5
@@ -53,9 +90,9 @@ def run():
 
     train_params = dict(
         accumulate_grad_batches=args.gradient_accumulation_steps,
-        gpus="2,3,4,5",
+        gpus=args.n_gpu,
         max_epochs=args.num_train_epochs,
-        early_stop_callback=False,
+        early_stop_callback=args.early_stop_callback,
         precision=16 if args.fp_16 else 32,
         amp_level=args.opt_level,
         gradient_clip_val=args.max_grad_norm,
