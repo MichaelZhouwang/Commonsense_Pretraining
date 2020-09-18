db99d932ecf6d1e839c8075249dc1eb80a7ecba0
Dongho Lee <danny911kr@naver.com>
2020-09-18 02:25:49 -0700

    Update mix_dataset.py

--- dataset_utils/mix_dataset.py
+++ dataset_utils/mix_dataset.py
@@ -1,18 +1,84 @@
-data = data2 = ""
+merge_source = concept_source = keyword_source = option2_source = ""
 
-# Reading data from file1
-with open('file1.txt') as fp:
-    data = fp.read()
+with open('../datasets/concept_deshuffling/train.source') as fp:
+    concept_source = fp.read()
 
-# Reading data from file2
-with open('file2.txt') as fp:
-    data2 = fp.read()
+with open('../datasets/keyword_lm/train.source') as fp:
+    keyword_source = fp.read()
 
-# Merging 2 files
-# To add the data of file2
-# from next line
-data += "\n"
-data += data2
+with open('../datasets/option2/train.source') as fp:
+    option2_source = fp.read()
 
-with open('file3.txt', 'w') as fp:
-    fp.write(data)
+merge_source += concept_source
+merge_source += "\n"
+merge_source += keyword_source
+merge_source += "\n"
+merge_source += option2_source
+
+with open('../datasets/mix/train.source', 'w') as fp:
+    fp.write(merge_source)
+
+merge_target = concept_target = keyword_target = option2_target = ""
+
+with open('../datasets/concept_deshuffling/train.target') as fp:
+    concept_target = fp.read()
+
+with open('../datasets/keyword_lm/train.target') as fp:
+    keyword_target = fp.read()
+
+with open('../datasets/option2/train.target') as fp:
+    option2_target = fp.read()
+
+merge_target += concept_target
+merge_target += "\n"
+merge_target += keyword_target
+merge_target += "\n"
+merge_target += option2_target
+
+with open('../datasets/mix/train.target', 'w') as fp:
+    fp.write(merge_target)
+
+
+merge_source = concept_source = keyword_source = option2_source = ""
+
+with open('../datasets/concept_deshuffling/valid.source') as fp:
+    concept_source = fp.read()
+
+with open('../datasets/keyword_lm/valid.source') as fp:
+    keyword_source = fp.read()
+
+with open('../datasets/option2/valid.source') as fp:
+    option2_source = fp.read()
+
+merge_source += concept_source
+merge_source += "\n"
+merge_source += keyword_source
+merge_source += "\n"
+merge_source += option2_source
+
+with open('../datasets/mix/valid.source', 'w') as fp:
+    fp.write(merge_source)
+
+merge_target = concept_target = keyword_target = option2_target = ""
+
+with open('../datasets/concept_deshuffling/valid.target') as fp:
+    concept_target = fp.read()
+
+with open('../datasets/keyword_lm/valid.target') as fp:
+    keyword_target = fp.read()
+
+with open('../datasets/option2/valid.target') as fp:
+    option2_target = fp.read()
+
+merge_target += concept_target
+merge_target += "\n"
+merge_target += keyword_target
+merge_target += "\n"
+merge_target += option2_target
+
+with open('../datasets/mix/valid.target', 'w') as fp:
+    fp.write(merge_target)
+
+
+
+
